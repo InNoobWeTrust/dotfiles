8f45c86c730349fa7729ed19fbcc1cd6b0efd3db
InNoobWeTrust <vu.truong.16193@gmail.com>
2018-04-04 22:17:09 +0700

    Invoke update & upgrade & clean before install anything on fresh environment

--- remote_ide/setup.sh
+++ remote_ide/setup.sh
@@ -3,8 +3,10 @@ rm -rf "$(pwd)/dotfiles"
 git clone https://github.com/innoobwetrust/dotfiles.git && \
 cp -r dotfiles/.config "$HOME/.config" && \
 cp dotfiles/.tmux.conf $HOME/.tmux.conf && \
-sudo apt install tmux && \
+sudo apt update -y && \
+sudo apt upgrade -y && \
+sudo apt autoremove -y && \
+sudo apt autoclean -y && \
+sudo apt install tmux -y && \
 chmod +x setup_neovim.sh && \
 ./setup_neovim.sh && \
-chmod +x setup_flutter.sh && \
-./setup_flutter.sh
