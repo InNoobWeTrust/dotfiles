ee004259e390ba4f9d4c2fa2c73d1bcc904b552a
InNoobWeTrust <vu.truong.16193@gmail.com>
2022-10-27 22:52:55 +0700

    Add treesitter config for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -617,6 +617,24 @@ require('packer').startup(function()
     }
     --- coc.nvim
     --require('coc')
+
+    -- nvim-treesitter
+    require'nvim-treesitter.configs'.setup {
+        auto_install = true,
+        highlight = {
+            enable = true,
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            enable = true
+        },
+    }
+    o.foldmethod='expr'
+    o.foldexpr='nvim_treesitter#foldexpr()'
     ------------------------------------ End language server
 
     ------------------------------------------- Autocomplete
