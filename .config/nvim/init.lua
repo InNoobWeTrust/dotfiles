aa487918374213be8032debce790bbebf2dfbf9e
InNoobWeTrust <vu.truong.16193@gmail.com>
2023-04-24 21:52:07 +0700

    Fix tab-autocomplete for Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -520,6 +520,17 @@ require('packer').startup(function()
 	})
 	--------------------------------------------- End linter
 
+	--------------------------------------------- Completion
+	local has_words_before = function()
+		unpack = unpack or table.unpack
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+
+	local feedkey = function(key, mode)
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+	end
+
 	local cmp = require('cmp')
 	cmp.setup({
 		snippet = {
@@ -527,18 +538,34 @@ require('packer').startup(function()
 				fn['vsnip#anonymous'](args.body)
 			end,
 		},
-		mapping = {
+		mapping = cmp.mapping.preset.insert({
 			--['<C-y>'] = cmp.mapping.confirm({ select = true }),
-			['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-			['<C-d>'] = cmp.mapping.scroll_docs(-4),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif vim.fn["vsnip#available"](1) == 1 then
+					feedkey("<Plug>(vsnip-expand-or-jump)", "")
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function()
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+					feedkey("<Plug>(vsnip-jump-prev)", "")
+				end
+			end, { "i", "s" }),
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
 			['<C-Space>'] = cmp.mapping.complete(),
-			['<C-e>'] = cmp.mapping.close(),
+			['<C-e>'] = cmp.mapping.abort(),
 			['<CR>'] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
 				select = true,
 			})
-		},
+		}),
 		sources = {
 			{ name = 'async_path' },
 			{ name = 'buffer' },
@@ -583,7 +610,7 @@ require('packer').startup(function()
 			capabilities = capabilities,
 		}
 	end
-	--------------------------------------- End Autocomplete
+	----------------------------------------- End completion
 
 	--------------------------------- Editor tooling manager
 	require("mason").setup()
