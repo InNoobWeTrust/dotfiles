65b3d9107d080acbffe7321cd44ca10b86cf47c5
InNoobWeTrust <forbidden.gun@gmail.com>
2024-03-15 05:58:09 +0700

    Fix zsh hooks for pkgx

--- .sh.d/.zsh.d/hooks.zsh
+++ .sh.d/.zsh.d/hooks.zsh
@@ -1,7 +1,5 @@
-#!/usr/bin/env zsh
-
 # Check if zoxide is usable and then source the hooks from zoxide.zsh
-usable zoxide && . "${0:A:h}/zoxide.zsh"
+usable zoxide && . "$CONF_ZSH_DIR/zoxide.zsh"
 
 # Check if pkgx is usable and then source the hooks from pkgx.bash
-usable pkgx && . "$CONF_BASH_DIR/pkgx.zsh"
+usable pkgx && . "$CONF_ZSH_DIR/pkgx.zsh"
