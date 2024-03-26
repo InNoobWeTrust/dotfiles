f16f3b95d790e1cefcbea2f887546323f0b3430e
InNoobWeTrust <vu.truong.16193@gmail.com>
2024-03-27 01:51:30 +0700

    Fix displaying elapsed time for each pressed target

--- life_hacks/social-pressing/src/main.rs
+++ life_hacks/social-pressing/src/main.rs
@@ -78,6 +78,7 @@ async fn report(links: &[String], cookies: &[Cookie<'_>]) -> Result<(), Box<dyn 
             }
         }
 
+        let pressing_start = Instant::now();
         match &*host_str {
             "www.facebook.com" | "facebook.com" => {
                 if limitation_status["fb"] {
@@ -120,13 +121,14 @@ async fn report(links: &[String], cookies: &[Cookie<'_>]) -> Result<(), Box<dyn 
                 warn!(target: "report", "Unrecognized url for {target} <{comment}>. Skipping...");
             }
         }
-        let elapsed = start.elapsed();
+        let elapsed = pressing_start.elapsed();
         let elapsed_str = humantime::format_duration(elapsed);
-        if elapsed > Duration::from_secs(2 * 3600) {
+        info!(target: "report", "Took: {elapsed_str}");
+
+        let elapsed_total = start.elapsed();
+        if elapsed_total > Duration::from_secs(2 * 3600) {
             warn!(target: "report", "{elapsed_str} exceeded 2 hours, stopping early...");
             break;
-        } else {
-            info!(target: "report", "Took: {elapsed_str}");
         }
     }
     let total = humantime::format_duration(start.elapsed());
